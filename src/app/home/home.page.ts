154e533a88bfb03e398827dc8be5d1a82c6a1e70
Aatif-khan <pathanaatif6@gmail.com>
2020-06-30 18:19:58 +0530

    second commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { EventResponse } from "../interfaces";
+import { Subscription } from "rxjs";
+import { EventsService } from "../events.service";
+import { NavController } from "@ionic/angular";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
 })
-export class HomePage {
+export class HomePage implements OnInit, OnDestroy {
+  events: EventResponse[] = [];
+  sub: Subscription;
+  constructor(private eventService: EventsService, private nav: NavController) {
+    console.log("constructor");
+  }
+  ngOnInit(): void {
+    console.log("ngOnInit");
 
-  constructor() {}
+    this.sub = this.eventService.getAll().subscribe((e) => this.events.push(e));
+  }
+  ngOnDestroy(): void {
+    console.log("ngOnDestroy");
 
+    this.sub.unsubscribe();
+  }
+  getEvents(): EventResponse[] {
+    console.log("getEvents");
+
+    return this.events.sort((a, b) =>
+      a.event.created > b.event.created ? -1 : 1
+    );
+  }
+  details(response: EventResponse) {
+    console.log("details");
+
+    this.nav.navigateForward(`/details/${response.event.id}`);
+  }
 }
